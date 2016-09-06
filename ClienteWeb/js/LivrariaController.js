3f09114e5111d8a55087ff468e1cf132682d2fe9
MarioIgor <marioigormenezes@gmail.com>
2016-09-06 11:38:11 -0300

    Exercicio 1
    Exercicio 1 sobre HTTP.

--- ClienteWeb/js/LivrariaController.js
+++ ClienteWeb/js/LivrariaController.js
@@ -3,20 +3,23 @@ app.controller('LivrariaController', function ($scope, $http) {
     $scope.livro = {};
     $scope.listaLivros = [];
 
-    $scope.adiciona = function () {
+    $scope.adiciona = function (form) {
+        if(!form.$valid) {
+            return;
+        }
+
         $scope.livro.id = $scope.listaLivros.lenght+1;
 
         $http.post('http://localhost:3000/livros', $scope.livro).success(
             function (dados) {
                 $scope.listaLivros.push($scope.livro);
                 $scope.livro = {};
+                $scope.lista();
             }
         );
     };
 
-
     $scope.lista = function () {
-
         $http.get('http://localhost:3000/livros').success(
             function (dados) {
                 $scope.listaLivros = dados;
@@ -24,6 +27,30 @@ app.controller('LivrariaController', function ($scope, $http) {
         );
     };
 
+    $scope.atualiza = function (livro) {  
+        if (!$scope.formEdicao.$valid) return;
+
+        $http.put('http://localhost:3000/livros/'+livro.id, livro).success(
+            function (dados) {
+                $scope.editando = false;   
+            }
+        );                          
+    };
+
+    $scope.apaga = function (livro) {        
+        $http.delete('http://localhost:3000/livros/'+livro.id).success(
+            function (dados) {
+                $scope.lista();
+            }
+        );                 
+    };
+
+    $scope.edita = function (livro) {
+        $scope.editando = true;  
+        $scope.idEdicao = livro.id; 
+    };
+
     $scope.lista();
+    $scope.editando = false;    
 
 });
